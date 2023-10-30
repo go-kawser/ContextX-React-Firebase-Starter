6e8c8fbe156d34e733672bea177ba4a7e5da3fbc
Kawser420 <muhammadkawser202@gmail.com>
2023-10-30 14:35:07 +0600

    i make a new Email-Passwoerd-Firebase Combination created by Kawser

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg_9AX7jlf8RQ11HHdMRQWBBQG0P74sdA",
+  authDomain: "auth-firebase-context-ta-9a0ed.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-9a0ed",
+  storageBucket: "auth-firebase-context-ta-9a0ed.appspot.com",
+  messagingSenderId: "9298232811",
+  appId: "1:9298232811:web:65c779e3c8d32a441585ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
