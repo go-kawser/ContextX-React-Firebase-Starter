96bd15c7b978ad2632e2c5e54a35cbc7abb1b4e9
Kawser420 <muhammadkawser202@gmail.com>
2023-11-01 20:47:06 +0600

    implemet signOut

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -14,15 +14,33 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const signIn = (email, password) =>{
+    const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {  
+        return signOut(auth);
     }
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
+    // observe auth state change
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser)
+            setUser(currentUser);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+
+
+    }, [])
 
     return (
         <AuthContext.Provider value={authInfo}>
