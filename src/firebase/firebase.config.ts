02b1ecca9291d12810c40d97c89b6cc752447acc
Kawser420 <muhammadkawser202@gmail.com>
2025-09-05 00:19:48 +0600

    updated new tech and analysic code

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,4 +1,4 @@
-// Import the functions
+// Import The functions
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
